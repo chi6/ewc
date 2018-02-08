aaff2243e0310f850381be7997eb0cb5e59fa032
asinugobi <asinugo@seas.upenn.edu>
2018-02-08 16:30:49 -0500

    first working implementation of training mnist w/ simple cnn

--- network.py
+++ network.py
@@ -33,32 +33,28 @@ class SimpleCNN(object):
 
     def create_graph(self):
         # First layer 
-        conv1 = conv2d(self.X, 5, 5, 32, 1, 1, name='conv1', padding='SAME')
-        pool1 = max_pool(conv1, 2, 2, 2, 2, padding='SAME', name='pool1')
+        # Reshape the images to [BATCH_SIZE, 28, 28, 1] to make it work with tf.nn.conv2d
+        images = tf.reshape(self.X, shape=[-1, 28, 28, 1])
+        conv1 = self.conv2d(images, 5, 5, 32, 1, 1, name='conv1', padding='SAME')
+        pool1 = self.max_pool(conv1, 2, 2, 2, 2, name='pool1', padding='SAME')
         
         # Second layer 
-        conv2 = conv2d(pool1, 5, 5, 64, 1, 1, name='conv2', padding='SAME')
-        pool2 = max_pool(conv2, 2, 2, 2, 2, padding='SAME', name='pool2')
+        conv2 = self.conv2d(pool1, 5, 5, 64, 1, 1, name='conv2', padding='SAME')
+        pool2 = self.max_pool(conv2, 2, 2, 2, 2, name='pool2', padding='SAME')
 
         # Fully-connected layer 
-        output_shape = int(pool2.get_shape()) 
+        output_shape = pool2.get_shape()
         height = int(output_shape[1])
         width = int(output_shape[2])
         num_filters = int(output_shape[-1])
-        fc1 = fc(input=pool2, input_features=height*width*num_filters, 1024, name='fc1')
+        fc1 = self.fc(input=pool2, input_features=height*width*num_filters, num_output_units=1024, name='fc1')
 
         # Softmax (prediction) layer
-        self.sigma = softmax(input=fc1, num_input_units=1024, name='softmax_linear')
+        self.sigma = self.softmax(input=fc1, num_input_units=1024, name='softmax_linear')
     
     def conv2d(self, input, filter_height, filter_width, num_filters, stride_y, stride_x, name, padding='SAME'): 
         # Get number of input channels 
         input_channels = int(input.get_shape()[-1])
-
-        # Create lambda function for the convolution 
-        convolve = lambda i, k: tf.nn.conv2d(input=i, 
-                                             filter=k,
-                                             strides=[1, stride_y, stride_x, 1],
-                                             padding=padding)
 
         # Create tf variables for the weights and biases of the conv layer 
         with tf.variable_scope(name) as scope: 
@@ -69,10 +65,13 @@ class SimpleCNN(object):
                                      shape=[num_filters], 
                                      initializer=tf.truncated_normal_initializer())
 
-            conv = convolve(input, kernel)
+            conv = tf.nn.conv2d(input=input, 
+                                filter=kernel,
+                                strides=[1, 1, 1, 1], 
+                                padding='SAME')
             return tf.nn.relu(conv + biases, name=scope.name)
 
-    def max_pool(self, input, filter_height, filter_width, stride_x, stride_y, padding='SAME', name):
+    def max_pool(self, input, filter_height, filter_width, stride_x, stride_y, name, padding='SAME'):
         with tf.variable_scope(name) as scope: 
             pool = tf.nn.max_pool(value=input,
                                   ksize=[1, filter_height, filter_width, 1],
@@ -96,16 +95,16 @@ class SimpleCNN(object):
             fc = tf.nn.relu(tf.matmul(input, w) + b, name='relu') 
 
             # Apply dropout.
-            fc = tf.nn.dropout(fc, dropout, name='relu_dropout')
+            fc = tf.nn.dropout(fc, self.DROPOUT, name='relu_dropout')
             return fc 
 
     def softmax(self, input, num_input_units, name): 
         with tf.variable_scope(name) as scope: 
             w = tf.get_variable(name='weights', 
-                                shape=[num_input_units, N_CLASSES],
+                                shape=[num_input_units, self.NUM_CLASSES],
                                 initializer=tf.truncated_normal_initializer())
             b = tf.get_variable(name='biases', 
-                                shape=[N_CLASSES], 
+                                shape=[self.NUM_CLASSES],
                                 initializer=tf.random_normal_initializer())
             logits = tf.matmul(input, w) + b
             return logits  
---
+++ utils.py
@@ -0,0 +1,12 @@
+"""
+Utility functions for convnets. 
+"""
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+def make_dir(path):
+    """ Create a directory if there isn't one already. """
+    try:
+        os.mkdir(path)
+    except OSError:
+    	pass
