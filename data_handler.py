b04809a299af2d9e846ca5353c5dd8eedc3fb468
Ryan <ryanoldja@gmail.com>
2018-02-08 19:40:01 -0500

    fixed bugs with integration of data handler

--- data_handler.py
+++ data_handler.py
@@ -1,16 +1,17 @@
 from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets, DataSet
+from tensorflow.python.framework import dtypes
 from tensorflow.contrib.learn.python.learn.datasets import base
 
 import numpy as np 
 
 class DataHandler(object): 
-	def __init__(self, dataset):
+    def __init__(self, dataset):
         self.set_dataset(dataset)
-
-	def set_dataset(self): 
-		if dataset == 'mnist':
+        
+    def set_dataset(self, dataset): 
+        if dataset == 'mnist':
             self.dataset = read_data_sets('./data/mnist/', one_hot=True)
-
+    
     def get_dataset(self): 
         return self.dataset
 
@@ -18,14 +19,16 @@ class DataHandler(object):
         labels = self.dataset.train.labels  
 
         # Find all training images/labels 1-4 
-        train_labels = np.nonzero(self.dataset.train.labels)[1]
-        train_labels = np.nonzero(train_labels < 5)[0]
-        train_images = self.dataset.train.images[train_labels]
+        train_labels_idx = np.nonzero(self.dataset.train.labels)[1]
+        train_labels_idx = np.nonzero(train_labels_idx < 5)[0]
+        train_labels = self.dataset.train.labels[train_labels_idx]
+        train_images = self.dataset.train.images[train_labels_idx]
 
         # Find all testing images/labels 1-4 
-        test_labels = np.nonzero(self.dataset.test.labels)[1]
-        test_labels = np.nonzero(test_labels < 5)[0]
-        test_images = self.dataset.test.images[test_labels]
+        test_labels_idx = np.nonzero(self.dataset.test.labels)[1]
+        test_labels_idx = np.nonzero(test_labels_idx < 5)[0]
+        test_labels = self.dataset.test.labels[test_labels_idx] 
+        test_images = self.dataset.test.images[test_labels_idx] 
 
         # Create validation/training groups 
         validation_images = train_images[:validation_size]
@@ -33,7 +36,7 @@ class DataHandler(object):
         train_images = train_images[validation_size:]
         train_labels = train_labels[validation_size:]
 
-        options = dict(dtype=dtype, reshape=reshape, seed=seed)
+        options = dict(dtype=dtype, reshape=False, seed=seed)
 
         # Define training, validation, and testing datasets  
         train = DataSet(train_images, train_labels, **options)
