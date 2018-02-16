ff920b9baa643c654ba17cfd755147265029004c
asinugobi <asinugo@seas.upenn.edu>
2018-02-16 14:48:16 -0500

    fixed bugs

--- mnist/model.py
+++ mnist/model.py
@@ -5,7 +5,7 @@ class Model(object):
     def __init__(self, classifier):
         self.classifier = classifier 
 
-    def compute_fisher(self, x, dropout, dataset, sess, num_samples=200): 
+    def compute_fisher(self, trainer, dataset, sess, num_samples=200): 
         """
         Compute Fisher information matrix
         """
@@ -15,46 +15,75 @@ class Model(object):
         self.variable_list = tf.trainable_variables() 
         for var in range(len(self.variable_list)): 
             self.F_matrix.append(np.zeros(self.variable_list[var].get_shape().as_list()))
+
+        # scores = self.classifier.get_scores() 
+        # results = sess.run(scores, feed_dict={
+        #     trainer.x: dataset.images[0:1],
+        #     trainer.y: dataset.labels[0:1],
+        #     trainer.dropout: 0.75, 
+        #     trainer.phase: 1})
+        # print(results)
         
         # Sample from a random class from softmax 
-        scores = tf.nn.softmax(self.classifier.get_scores())
-        class_ind = tf.to_int32(tf.multinomial(tf.log(scores), 1)[0][0])
-        print('Class index: %s', class_ind)
-        # x = tf.placeholder(dtype=tf.float32, shape=[None, 784], name='x_placeholder')
+        # scores = tf.nn.softmax(self.classifier.get_scores())
+        scores = tf.nn.softmax_cross_entropy_with_logits(labels=trainer.y, logits=self.classifier.get_scores())
+        # class_ind = tf.to_int32(tf.multinomial(tf.log(scores), 1)
+        # [0][0])
+
+        results = sess.run([scores], feed_dict={
+            trainer.x: dataset.images[0:5],
+            trainer.y: dataset.labels[0:5],
+            trainer.dropout: 0.75, 
+            trainer.phase: 1})
+        print(results)
+
+        # Initialize loss function 
+        # self.cross_entropy_loss(y)
+        # log_likelihood = self.loss 
 
         with sess.as_default(): 
-            sess.run(tf.global_variables_initializer())
-            test = sess.run(tf.log(scores[0, class_ind]), feed_dict={dropout: 0.75}) 
-            print(test)
+        #     sess.run(tf.global_variables_initializer())
+            # image_idx = np.random.randint(dataset.shape[0])
+            results = sess.run(tf.log(scores), feed_dict={
+            trainer.x: dataset.images[0:5],
+            trainer.y: dataset.labels[0:5],
+            trainer.dropout: 0.75, 
+            trainer.phase: 1})
+            print('log results: %s', results)
 
             # Compute Fisher information matrix 
             for idx in range(num_samples): 
                 # Select input image randomly 
-                image_idx = np.random.randint(dataset.shape[0])
-                print(image_idx)
-
-                if hasattr(self, "variable_list"): 
-                    print("Has variables")
-                    print(len(self.variable_list))
-                else: 
-                    print("No variables")
+                image_idx = np.random.randint(dataset.images.shape[0])
 
                 # Compute first-order derivatives
                 # Consider using log likelihood as an alternative implementation  
+                derivatives = sess.run(tf.gradients(ys=tf.log(scores), xs=self.variable_list), 
+                feed_dict={
+                    trainer.y: dataset.labels[image_idx:image_idx + 5],
+                    trainer.x: dataset.images[image_idx:image_idx + 5], 
+                    trainer.phase: 1, 
+                    trainer.dropout: 0.75})
+                print(derivatives)
 
-                # derivatives = sess.run(tf.gradients(tf.log(scores[0, class_ind]), self.variable_list), feed_dict={x: dataset[image_idx:image_idx + 1]})
+                # gradient_op = tf.gradients(-log_likelihood, self.variable_list)
 
-                # log_likelihood = self.cross_entropy_loss(self.classifier.get_scores())
+                # derivatives = sess.run(gradient_op, 
+                # feed_dict={
+                #     y: dataset.labels[image_idx:image_idx + 1],
+                #     trainer.x: dataset.images [image_idx:image_idx + 1], 
+                #     trainer.phase: 0, 
+                #     trainer.dropout: 0.0})
+
 
                 # Square the derivatives and add to the total 
-                for var in range(len(self.F_matrix)):
-                    self.F_matrix[var] += np.square(derivatives[var])
+                # for var in range(len(self.F_matrix)):
+                #     self.F_matrix[var] += np.square(derivatives[var])
 
         # Divide by the total number of sample 
-        for var in range(len(self.F_matrix)): 
-            self.F_matrix[var] /= num_samples 
+        # for var in range(len(self.F_matrix)): 
+        #     self.F_matrix[var] /= num_samples 
 
-    
     def save_weights(self, sess):
         """
         Save weights after training source task. 
@@ -72,8 +101,10 @@ class Model(object):
         """
         Elastic weight consolidation. 
         """
-        self.ewc_loss = self.cross_entropy_loss(y)
-
+        # Initialize loss function 
+        self.cross_entropy_loss(y)
+        self.ewc_loss = self.loss
+        
         for var in range(len(self.variable_list)): 
             self.ewc_loss += (fisher_multiplier / 2) * tf.reduce_sum(tf.multiply(self.F_matrix[var].astype(np.float32),tf.square(self.variable_list[var] - self.star_vars[var])))  
     
